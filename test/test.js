13294564f4e71a946a80a716fa8f312164b00ab2
tom139 <tommasopanozzo@gmail.com>
2016-08-03 16:33:04 +0200

    Removed fake tests

--- test/test.js
+++ test/test.js
@@ -1,42 +1,9 @@
 'use strict';
 
 const expect = require('chai').expect;
-const clips  = require('../index.js');
 
-describe('#clips', function() {
-  it('should make 3 times 2', function() {
-    var result = clips(3);
-    expect(result).to.equal(6);
+describe('# fake test', function() {
+  it('should be true', function() {
+    expect(true).to.equal(true);
   });
-
-  it('should make 2 times 2', function() {
-    var result = clips(2.0);
-    expect(result).to.equal(4.0);
-  });
-});
-
-describe('#path', function() {
-   const ProofHandler = require('../server/Path.js');
-   var handler = new ProofHandler();
-   var proof = {
-      fakeProof: "fake",
-      testData:  "BeaconStripsVF.json",
-      scoringAlgorithmData: "5050.json"
-   };
-   var fulfilled = handler.fulfilledProof(proof);
-   it('should retrieve test', function() {
-      fulfilled.then(function(proof) {
-         console.log('proof: ', proof);
-         proof.should.have.property('test');
-         proof.test.should.be.a('string');
-      });
-   });
-
-   it('should retrieve algorithm', function() {
-      fulfilled.then(function(proof) {
-         console.log(proof);
-         proof.should.have.property('algorithm');
-         proof.algorithm.should.be.a('string');
-      });
-   });
 });
