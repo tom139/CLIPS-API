e14c6f02fdd15b72a0e05861614d03b6e4755dad
tom139 <tommasopanozzo@gmail.com>
2016-08-04 10:40:45 +0200

    Fix Tab spaces

--- test/test.js
+++ test/test.js
@@ -3,7 +3,10 @@
 const expect = require('chai').expect;
 
 describe('# fake test', function() {
-  it('should be true', function() {
-    expect(true).to.equal(true);
-  });
+   it('should be true', function() {
+      expect(true).to.equal(true);
+   });
+   it('should be false', function() {
+      expect(false).to.equal(false);
+   });
 });
