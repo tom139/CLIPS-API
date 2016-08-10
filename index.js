ad8baf24aa1bbab83ee21de559daef53e2adf7f8
Tommaso Panozzo <tommasopanozzo@gmail.com>
2016-08-10 19:52:05 +0200

    Add: intestazione per index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/**
+*  @file index.js
+*  @date 01/08/2016
+*  @version 1
+*  @author Tommaso Panozzo
+*
+*  entry point dell'API
+*/
+
 'use strict';
 
 const server = require('./server/server.js');
