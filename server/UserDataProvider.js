23ccca141d50423b9966e017eda19e388c524e69
Tommaso Panozzo <tommasopanozzo@gmail.com>
2016-08-22 09:36:32 +0200

    Update: add logging

--- server/UserDataProvider.js
+++ server/UserDataProvider.js
@@ -78,7 +78,8 @@ function GetUserData() {
    };
 
    this.sendData = function(data) {
-      console.log('response = ', this.response);
+      console.log(this);
+      // console.log('response = ', this.response);
       console.log('sendData(',data,')');
       this.response.status(200).send(data);
    };
