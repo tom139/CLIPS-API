c361fd3b6925b5ec0907b57072febb776260d734
Tommaso Panozzo <tommasopanozzo@gmail.com>
2016-08-31 08:35:47 +0200

    Fix: Typo in emailSender

--- server/EmailSender.js
+++ server/EmailSender.js
@@ -5,13 +5,6 @@ const config     = require('./config.js');
 
 const transporter = nodemailer.createTransport('smtps://beaconstrips.swe%40gmail.com:beaconclips2016swe@smtp.gmail.com');
 
-transporter.sendMail(mail, function(error, info) {
-   if (error) {
-      console.error(error);
-   }
-   console.log('info:', info);
-});
-
 function sendMail(mail) {
    var options = mail;
    options.from = '"BeaconStrips" <beaconstrips.swe@gmail.com>';
