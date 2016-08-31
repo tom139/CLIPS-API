7ac9e152b4651cab50ec0f4e975bcde8cce39538
Tommaso Panozzo <tommasopanozzo@gmail.com>
2016-08-31 09:19:55 +0200

    Update: update transportation to bypass port 25

--- server/EmailSender.js
+++ server/EmailSender.js
@@ -3,7 +3,14 @@
 const nodemailer = require('nodemailer');
 const config     = require('./config.js');
 
-const transporter = nodemailer.createTransport('smtps://beaconstrips.swe%40gmail.com:beaconclips2016swe@smtp.gmail.com');
+// const transporter = nodemailer.createTransport('smtps://beaconstrips.swe%40gmail.com:beaconclips2016swe@smtp.gmail.com');
+const transporter = nodemailer.createTransport({
+   service: 'gmail',
+   auth: {
+      user: 'beaconstrips.swe@gmail.com',
+      pass: 'beaconclips2016swe'
+   }
+});
 
 function sendMail(mail) {
    var options = mail;
