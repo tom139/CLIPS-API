cf75501295f65caebc9f784366f9754812ceb18d
Tommaso Panozzo <tommasopanozzo@gmail.com>
2016-09-03 13:16:49 +0200

    Removed comment

--- test/test.js
+++ test/test.js
@@ -12,50 +12,6 @@ const port = config.port;
 
 require('./testAppInfo.js');
 
-/*
-const appinfo = {
-   uri: 'http://' + host + ':' + port + '/appinfo',
-   json: true
-};
-
-describe('# AppInfo', function() {
-   it('request AppInfo', function() {
-      return request(appinfo)
-      .then(function(response) {
-         describe('should return website URL', function() {
-            it('website URL is string', function() {
-               expect(response.websiteURL).to.be.a('string');
-            });
-         });
-         describe('should return description', function() {
-            it('description is string', function() {
-               expect(response.description).to.be.a('string');
-            });
-         });
-         describe('should return support email', function() {
-            const email = response.supportemail
-            it('* email is string', function() {
-               expect(email).to.be.a('string');
-            });
-            it('* email is valid', function() {
-               expect(validator.validate(email)).to.equal(true);
-            });
-         });
-         describe('should return discovery UUID', function() {
-            const uuid = response.discoveryUUID;
-            it('* UUID is string', function() {
-               expect(uuid).to.be.a('string');
-            });
-            it('* UUID have length 37', function() {
-               expect(uuid).to.have.length(37);
-            });
-         });
-      });
-   });
-});
-
-*/
-
 const buildings = {
    uri: 'http://' + host + ':' + port + '/buildings',
    method: 'POST',
