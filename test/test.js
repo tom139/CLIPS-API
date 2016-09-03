1c9adc9f251ab254e3f6d87a8e53c59c24329733
Tommaso Panozzo <tommasopanozzo@gmail.com>
2016-09-03 13:45:49 +0200

    Update: removed unusefull dependency

--- test/test.js
+++ test/test.js
@@ -1,17 +1,10 @@
 'use strict';
 
-const request = require('request-promise');
-const expect = require('chai').expect;
-const assert = require('chai').assert;
-const validator = require('email-validator');
-const index = require('./../index.js');
-const config = require('./test_config.js');
-
-const host = config.host;
-const port = config.port;
-
+// test app info
 require('./testAppInfo.js');
 
+// test buildings
 require('./testBuildings.js');
 
+// test user flow (Registration, user data display, user data modification, login)
 require('./testUserFlow');
