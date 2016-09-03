3a4fb0feb73312657bde012f7a5a55119ab39fb2
Tommaso Panozzo <tommasopanozzo@gmail.com>
2016-09-03 13:41:39 +0200

    Renamed method

--- test/helperFunctions.js
+++ test/helperFunctions.js
@@ -7,8 +7,12 @@ const config = require('./test_config.js');
 
 module.exports.baseURI = 'http://' + config.host + ':' + config.port;
 
-exports.shouldFail = function (response) {
+exports.clientError = function (response) {
    expect(response.statusCode).to.be.least(400).and.to.be.below(500);
+};
+
+exports.shouldFail = function (response) {
+   exports.clientError(response);
 };
 
 exports.fail = function () {
