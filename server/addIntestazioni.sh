50d8f21fa5abdfecb35ef5a40b7cfed97711d39e
Tommaso Panozzo <tommasopanozzo@gmail.com>
2016-09-09 10:12:15 +0200

    Add date

--- server/addIntestazioni.sh
+++ server/addIntestazioni.sh
@@ -11,4 +11,4 @@ find . -name "*.js" -exec sh -c "echo -e ' * @date '"
 
 
 
-find . -name "*.js" -exec sh -c "echo '/**\n * @file {}\n * @date `git log --format=%aD {} | tail -1`\n * @version 1.0\n * @author Tommaso Panozzo\n * \n * DescrizioneDelFile\n */ '|cat - {} > /tmp/out && mv /tmp/out {}" \;
+find . -name "*.js" -exec sh -c "echo '/**\n * @file {}\n * @date '; git log --format=%aD {} | tail -1; echo '\n * @version 1.0\n * @author Tommaso Panozzo\n * \n * DescrizioneDelFile\n */ '|cat - {} > /tmp/out && mv /tmp/out {}" \;
