152f9a96496e33d1d2f46b0e7d9f5719f94aca1b
Tommaso Panozzo <tommasopanozzo@gmail.com>
2016-09-11 21:33:28 +0200

    unused classes cleanup

--- test/test_PasswordChecker.js
+++ test/test_PasswordChecker.js
@@ -8,7 +8,9 @@
  */
 'use strict';
 
-var passwordChecker = require('./PasswordChecker.js');
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+var passwordChecker = require('../server/utility/PasswordChecker.js');
 
 console.log('start test...\n\n');
 
@@ -43,19 +45,11 @@ const tests = [
    }
 ]
 
-var testCounter = 0;
-var passedCounter = 0;
-var failedCounter = 0;
-for (var test of tests) {
-   var result = passwordChecker.isValid(test.password);
-   if (result == test.expected) {
-      console.log(testCounter + ' - [PASSED]: ' + test.password);
-      passedCounter++;
-   } else {
-      console.error(testCounter + ' - [FAILED]: ' + test.password + ' expected: ' + test.expected + ', result: ' + result);
-      failedCounter++;
-   }
-   testCounter++;
-}
-
-console.log('\n\n FAILED: ', failedCounter, ' PASSED: ', passedCounter, ' TOTAL: ', testCounter);
+describe('# password validator', function() {
+   it('should behave as expected', function() {
+      for (var test of tests) {
+         var result = passwordChecker.isValid(test.password);
+         expect(result).to.be.equal(test.expected);
+      }
+   });
+});
